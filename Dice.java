7765269eb5ba816586a1ca2c7a23f896b4c25b85
TheFlash1907 <20344023+TheFlash1907@users.noreply.github.com>
2020-04-30 17:31:24 +0300

    Dice update
    Fixed Dice random method.
    Added Dice animation (not tested).

---
+++ Dice.java
@@ -0,0 +1,55 @@
+package TravelRaceGame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Dice extends JButton
+{
+	// Date members:
+	private int result;
+	private String[] diceImages;
+	private Timer diceAnimation;
+	private int animationTime;
+	
+	public Dice()
+	{
+		super();
+		diceImages = new String[6];
+		SetRandomDice();
+		animationTime = 12;
+		//TODO
+		// sizes
+		
+		this.validate();
+	}
+	
+	public void SetRandomDice()
+	{
+		animateRandomImages();
+		diceAnimation.start();
+		this.setIcon(new ImageIcon(getClass().getResource(this.diceImages[result]))); // set random dice image
+	}
+	
+	private void animateRandomImages()
+	{		
+		diceAnimation = new Timer(250, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				animationTime--;
+				if (animationTime >= 0)
+				{
+					result = (int)(Math.random() * 6) + 1;
+					setIcon(new ImageIcon(getClass().getResource(diceImages[result])));
+					repaint();
+				}
+				else
+				{
+					diceAnimation.stop();
+				}
+			}
+		});
+	}
+}
