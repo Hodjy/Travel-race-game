6b6dfc1161145a4a1d7c0d080dc132231f8cde8f
NivAm12 <49129250+NivAm12@users.noreply.github.com>
2020-05-02 18:26:29 +0300

    Tile class
    Added the Tile class and his basic methods.

---
+++ Trave Race Game/src/TravelRaceGame/Tile.java
@@ -0,0 +1,25 @@
+package TravelRaceGame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Tile extends JLabel
+{
+	public Tile()
+	{
+		super();
+		// TODO: sizes of the tile, image icon
+	}
+	
+	// player avatar treatment:
+	public void AddPlayerAvatarOnTheTile(JLabel i_PlayerAvatar)
+	{
+		
+	}
+	
+	public void RemovePlayerAvatarFromTheTile()
+	{
+		
+	}
+}
