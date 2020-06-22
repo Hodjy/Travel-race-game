aadc04b18241b6aa47620a245735190844ea45e1
TheFlash1907 <matanz19@gmail.com>
2020-06-22 15:12:53 +0300

    view interface

--- Trave Race Game/src/TravelRaceGame/View/View.java
+++ Trave Race Game/src/TravelRaceGame/View/View.java
@@ -1,10 +1,13 @@
 package TravelRaceGame.View;
 import java.util.Observable;
 
-
-
-
 public interface View
 {
-	
+	public int GetCardClickedIndex();
+	public BoardPanel GetBoard();
+	public void Initilize();
+	public void EnablePlayButtons(boolean i_Enable);
+	public void EnableCardsInHandClick(boolean i_Enable);
+	public void EnableDiceButton(boolean i_Enable);
+	public void SetCardsInHandAndEnableEvents(String[] i_HandToSet);
 }
